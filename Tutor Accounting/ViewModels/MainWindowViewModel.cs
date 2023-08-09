147bf9b457a42d75aa1c1948a03e26792a4dbc88
Maks Harunzhin <maskim.kharunzhin@mail.ru>
2023-08-09 20:19:52 +0300

    Первый ViewModel

---
+++ Tutor Accounting/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutor_Accounting.ViewModels.Base;
+
+namespace Tutor_Accounting.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Properties
+
+        private double _columnWidth = 300;
+        /// <summary>
+        /// Ширина столбца с настройками календаря
+        /// </summary>
+        public double ColumnWidth
+        {
+            get => _columnWidth;
+            set => Set(ref _columnWidth, value);
+        }
+
+        #endregion
+
+        #region Commands
+
+
+
+        #endregion
+
+        /// <summary>
+        /// Конструктор класса MainWindowViewModel
+        /// </summary>
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
